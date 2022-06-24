0c226dccd3d7ae1568757f1e50c40f5013dda790
nohararyota <kokollmm2112@gmail.com>
2022-06-24 20:00:15 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,3 +3,9 @@ class ItemsController < ApplicationController
   end
 
 end
+
+
+  private
+  def image_params
+    params.require(:image).permit(:image).merge(user_id: current_user.id)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
+<<<<<<< Updated upstream
   resources :items, only: :index
+=======
+  resources :user, only: [:new] do
+    resources :items, only: [:index, :new]
+  end
+>>>>>>> Stashed changes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
