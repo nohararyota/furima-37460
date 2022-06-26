9839e3e09575f4c666877192c7c60854ec3533d2
nohararyota <kokollmm2112@gmail.com>
2022-06-26 21:48:18 +0900

    商品出品機能修正1

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,20 +8,18 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-
   validates :product_name,          presence: true
   validates :description_of_item,   presence: true
   validates :image,                 presence: true
 
   with_options presence: true do
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                      presence: { message: "can't be blank"}
+                      presence: { message: "can't be blank" }
   end
 
-  validates :category_id,         numericality: { other_than: 1, message: "can't be blank" } 
-  validates :situation_id,        numericality: { other_than: 1, message: "can't be blank" } 
-  validates :delivery_charge_id,  numericality: { other_than: 1, message: "can't be blank" } 
-  validates :prefecture_id,       numericality: { other_than: 1, message: "can't be blank" } 
-  validates :days_to_ship_id,     numericality: { other_than: 1, message: "can't be blank" } 
-
+  validates :category_id,         numericality: { other_than: 1, message: "can't be blank" }
+  validates :situation_id,        numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_charge_id,  numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,       numericality: { other_than: 1, message: "can't be blank" }
+  validates :days_to_ship_id,     numericality: { other_than: 1, message: "can't be blank" }
 end
