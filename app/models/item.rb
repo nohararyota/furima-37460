f84844a94855fa58e97e70488120f537eea44f20
nohararyota <kokollmm2112@gmail.com>
2022-06-26 21:15:32 +0900

    商品出品機能修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
   validates :description_of_item,   presence: true
   validates :image,                 presence: true
 
-  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+  with_options presence: true do
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
                       presence: { message: "can't be blank"}
   end
