8e460059b98af6802827f1de425ba9399ba8f05d
nohararyota <kokollmm2112@gmail.com>
2022-06-26 21:56:37 +0900

    商品出品機能２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
     # @item = Item.new(item_params)
     # if @item.save
     # redirect_to root_path
